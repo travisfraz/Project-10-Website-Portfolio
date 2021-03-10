42119f6abcba2b3e75b6202fcaac2193b2cdf412
travisfraz <travisfraz@gmail.com>
2021-03-10 09:17:41 -0600

    resume update

--- src/App/components/resume/ResumePage.js
+++ src/App/components/resume/ResumePage.js
@@ -6,7 +6,7 @@ export default function ResumePage() {
     return(
         <div className={style.resumePageContainer}>
             <h1 className={style.resumeTitle}>Travis' Resume</h1>
-            <iframe title='resume' className={style.resume} src="https://drive.google.com/file/d/1jJL8QLK1SEg_fX9Jng07hR6UkmDVr4g9/preview"></iframe>
+            <iframe title='resume' className={style.resume} src="https://drive.google.com/file/d/1n9-d_G51jev-9z22wEe1FMM1M7jKI-cZ/preview"></iframe>
         </div>
     )
 }
